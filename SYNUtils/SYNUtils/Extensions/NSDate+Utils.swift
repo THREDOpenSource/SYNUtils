6453b08f64713bae9a01d007705e65c837bba7e8
John Hurliman <jhurliman@jhurliman.org>
2015-06-26 22:28:10 -0700

    Made date string parsing thread safe

--- SYNUtils/SYNUtils/Extensions/NSDate+Utils.swift
+++ SYNUtils/SYNUtils/Extensions/NSDate+Utils.swift
@@ -9,41 +9,28 @@
 import Foundation
 
 extension NSDate: Comparable {
-    /// Returns the current system local timezone as the number of seconds
-    /// offset from GMT.
-    ///
-    /// :returns: Example: -25200 seconds for Pacific Daylight Time
-    public class func currentTZOffset() -> NSTimeInterval {
-        return NSTimeInterval(NSTimeZone.localTimeZone().secondsFromGMT)
-    }
-    
-    /// Returns the current system local timezone as the number of minutes
-    /// offset from GMT.
-    /// :returns: Example: -420 minutes for Pacific Daylight Time
-    public class func currentTZOffsetMinutes() -> Int {
-        return Int(currentTZOffset() / 60.0)
-    }
-    
-    // MARK: - Standard Date String Conversions
-    
-    struct DateFormatters {
-        static var isoFormatter: NSDateFormatter = {
+    static var isoFormatter: NSDateFormatter {
+        // NSDateFormatter is not thread-safe, so we allocate per thread
+        return lazyThreadLocalObject("SYNUtils.NSDate.isoFormatter") {
             let formatter = NSDateFormatter()
             formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z"
             formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
             formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
             formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierISO8601)!
             return formatter
-        }()
-        
-        static var rfc3339Formatter: NSDateFormatter = {
+        }
+    }
+    
+    static var rfc3339Formatter: NSDateFormatter {
+        // NSDateFormatter is not thread-safe, so we allocate per thread
+        return lazyThreadLocalObject("SYNUtils.NSDate.rfc3339Formatter") {
             let formatter = NSDateFormatter()
-            formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"
+            formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssXXX"
             formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
             formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
             formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierISO8601)!
             return formatter
-        }()
+        }
     }
     
     /// Initialize NSDate from a date string in either RFC 3339 or ISO 8601
@@ -60,13 +47,13 @@ extension NSDate: Comparable {
         }
         
         // Try RFC 3339 first
-        if let d = DateFormatters.rfc3339Formatter.dateFromString(dateString) {
+        if let d = NSDate.rfc3339Formatter.dateFromString(dateString) {
             self.init(timeInterval: 0, sinceDate: d)
             return
         }
         
         // Try ISO 8601 next
-        if let d = DateFormatters.isoFormatter.dateFromString(dateString) {
+        if let d = NSDate.isoFormatter.dateFromString(dateString) {
             self.init(timeInterval: 0, sinceDate: d)
             return
         }
@@ -75,18 +62,35 @@ extension NSDate: Comparable {
         return nil
     }
     
+    // MARK: - Standard Date String Conversions
+    
+    /// Returns the current system local timezone as the number of seconds
+    /// offset from GMT.
+    ///
+    /// :returns: Example: -25200 seconds for Pacific Daylight Time
+    public class func currentTZOffset() -> NSTimeInterval {
+        return NSTimeInterval(NSTimeZone.localTimeZone().secondsFromGMT)
+    }
+    
+    /// Returns the current system local timezone as the number of minutes
+    /// offset from GMT.
+    /// :returns: Example: -420 minutes for Pacific Daylight Time
+    public class func currentTZOffsetMinutes() -> Int {
+        return Int(currentTZOffset() / 60.0)
+    }
+    
     /// Returns this date as an ISO 8601 string.
     ///
-    /// :returns: ISO 8601 formatted string
+    /// :returns: ISO 8601 formatted string. Example:
     public func toISOString() -> String {
-        return DateFormatters.isoFormatter.stringFromDate(self)
+        return NSDate.isoFormatter.stringFromDate(self)
     }
     
     /// Returns this date as an RFC 3339 string.
     ///
     /// :returns: RFC 3339 formatted string
     public func toRFC3339String() -> String {
-        return DateFormatters.rfc3339Formatter.stringFromDate(self)
+        return NSDate.rfc3339Formatter.stringFromDate(self)
     }
     
     // MARK: - UNIX Timestamps
