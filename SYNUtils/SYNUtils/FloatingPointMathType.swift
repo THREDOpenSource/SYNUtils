32ba3bcfe3963888f42fcc11515489218a422569
John Hurliman <jhurliman@jhurliman.org>
2015-06-28 01:43:42 -0700

    Formatting

--- SYNUtils/SYNUtils/FloatingPointMathType.swift
+++ SYNUtils/SYNUtils/FloatingPointMathType.swift
@@ -20,7 +20,7 @@ public protocol FloatingPointMathType {
     var tan: Self {get}
     var exp: Self {get}
     var exp2: Self {get}
-    func lerp(a: Self, b: Self, t: Self) -> Self
+    func lerp(a: Self, _ b: Self, t: Self) -> Self
     var log: Self {get}
     var log10: Self {get}
     var log2: Self {get}
@@ -49,15 +49,15 @@ extension CGFloat: FloatingPointMathType {
     public var tan:CGFloat      { return CoreGraphics.tan(self) }
     public var exp:CGFloat      { return CoreGraphics.exp(self) }
     public var exp2:CGFloat     { return CoreGraphics.exp2(self) }
-    public func lerp(a: CGFloat, b: CGFloat, t: CGFloat) -> CGFloat { return a + (b - a) * t }
+    public func lerp(a: CGFloat, _ b: CGFloat, t: CGFloat) -> CGFloat { return a + (b - a) * t }
     public var log:CGFloat      { return CoreGraphics.log(self) }
     public var log10:CGFloat    { return CoreGraphics.log10(self) }
     public var log2:CGFloat     { return CoreGraphics.log2(self) }
-    public func pow(exponent: CGFloat)-> CGFloat { return CoreGraphics.pow(self, exponent) }
+    public func pow(exponent: CGFloat) -> CGFloat { return CoreGraphics.pow(self, exponent) }
     public var sqrt:CGFloat     { return CoreGraphics.sqrt(self) }
-    public var floor: CGFloat { return CoreGraphics.floor(self) }
-    public var ceil: CGFloat { return CoreGraphics.ceil(self) }
-    public var round: CGFloat { return CoreGraphics.round(self) }
+    public var floor: CGFloat   { return CoreGraphics.floor(self) }
+    public var ceil: CGFloat    { return CoreGraphics.ceil(self) }
+    public var round: CGFloat   { return CoreGraphics.round(self) }
     public func clamp(min: CGFloat, _ max: CGFloat) -> CGFloat {
         return Swift.max(min, Swift.min(max, self))
     }
@@ -72,24 +72,24 @@ extension Float: FloatingPointMathType {
         return Float(((rand / Double(RAND_MAX)) * diff) + min)
     }
     
-    public var abs: Float  { return Float.abs(self) }
-    public var acos: Float { return Foundation.acos(self) }
-    public var asin: Float { return Foundation.asin(self) }
-    public var atan: Float { return Foundation.atan(self) }
+    public var abs: Float   { return Float.abs(self) }
+    public var acos: Float  { return Foundation.acos(self) }
+    public var asin: Float  { return Foundation.asin(self) }
+    public var atan: Float  { return Foundation.atan(self) }
     public func atan2(x: Float) -> Float { return Foundation.atan2(self, x) }
-    public var cos: Float  { return Foundation.cos(self) }
-    public var sin: Float  { return Foundation.sin(self) }
-    public var tan: Float  { return Foundation.tan(self) }
-    public var exp: Float  { return Foundation.exp(self) }
-    public var exp2: Float { return Foundation.exp2(self) }
-    public func lerp(a: Float, b: Float, t: Float) -> Float { return a + (b - a) * t }
-    public var log: Float  { return Foundation.log(self) }
-    public var log10: Float{ return Foundation.log10(self) }
-    public var log2: Float { return Foundation.log2(self) }
-    public func pow(exponent: Float)-> Float { return Foundation.pow(self, exponent) }
-    public var sqrt: Float { return Foundation.sqrt(self) }
+    public var cos: Float   { return Foundation.cos(self) }
+    public var sin: Float   { return Foundation.sin(self) }
+    public var tan: Float   { return Foundation.tan(self) }
+    public var exp: Float   { return Foundation.exp(self) }
+    public var exp2: Float  { return Foundation.exp2(self) }
+    public func lerp(a: Float, _ b: Float, t: Float) -> Float { return a + (b - a) * t }
+    public var log: Float   { return Foundation.log(self) }
+    public var log10: Float { return Foundation.log10(self) }
+    public var log2: Float  { return Foundation.log2(self) }
+    public func pow(exponent: Float) -> Float { return Foundation.pow(self, exponent) }
+    public var sqrt: Float  { return Foundation.sqrt(self) }
     public var floor: Float { return Foundation.floor(self) }
-    public var ceil: Float { return Foundation.ceil(self) }
+    public var ceil: Float  { return Foundation.ceil(self) }
     public var round: Float { return Foundation.round(self) }
     public func clamp(min: Float, _ max: Float) -> Float {
         return Swift.max(min, Swift.min(max, self))
@@ -103,24 +103,24 @@ extension Double: FloatingPointMathType {
         return ((rand / Double(RAND_MAX)) * diff) + min
     }
     
-    public var abs: Double  { return Double.abs(self) }
-    public var acos: Double { return Foundation.acos(self) }
-    public var asin: Double { return Foundation.asin(self) }
-    public var atan: Double { return Foundation.atan(self) }
+    public var abs: Double   { return Double.abs(self) }
+    public var acos: Double  { return Foundation.acos(self) }
+    public var asin: Double  { return Foundation.asin(self) }
+    public var atan: Double  { return Foundation.atan(self) }
     public func atan2(x: Double) -> Double { return Foundation.atan2(self, x) }
-    public var cos: Double  { return Foundation.cos(self) }
-    public var sin: Double  { return Foundation.sin(self) }
-    public var tan: Double  { return Foundation.tan(self) }
-    public var exp: Double  { return Foundation.exp(self) }
-    public var exp2: Double { return Foundation.exp2(self) }
-    public func lerp(a: Double, b: Double, t: Double) -> Double { return a + (b - a) * t }
-    public var log: Double  { return Foundation.log(self) }
-    public var log10: Double{ return Foundation.log10(self) }
-    public var log2: Double { return Foundation.log2(self) }
-    public func pow(exponent: Double)-> Double { return Foundation.pow(self, exponent) }
-    public var sqrt: Double { return Foundation.sqrt(self) }
+    public var cos: Double   { return Foundation.cos(self) }
+    public var sin: Double   { return Foundation.sin(self) }
+    public var tan: Double   { return Foundation.tan(self) }
+    public var exp: Double   { return Foundation.exp(self) }
+    public var exp2: Double  { return Foundation.exp2(self) }
+    public func lerp(a: Double, _ b: Double, t: Double) -> Double { return a + (b - a) * t }
+    public var log: Double   { return Foundation.log(self) }
+    public var log10: Double { return Foundation.log10(self) }
+    public var log2: Double  { return Foundation.log2(self) }
+    public func pow(exponent: Double) -> Double { return Foundation.pow(self, exponent) }
+    public var sqrt: Double  { return Foundation.sqrt(self) }
     public var floor: Double { return Foundation.floor(self) }
-    public var ceil: Double { return Foundation.ceil(self) }
+    public var ceil: Double  { return Foundation.ceil(self) }
     public var round: Double { return Foundation.round(self) }
     public func clamp(min: Double, _ max: Double) -> Double {
         return Swift.max(min, Swift.min(max, self))
